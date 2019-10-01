736b6dcb175a301ba413ff3c5bbca0d5012035f0
Gabriel Gigante <gabriel_gigante@outlook.com>
2019-10-01 14:57:36 -0300

    Finished API

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-fc7zy.mongodb.net/omnistack9?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//GET, POST, PUT, DELETE
+
+//req.query = acessar query params (para filtros)
+//req.params = acessar route params (para edicao, delete)
+//req.body = acessar corpo da requisicao
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
