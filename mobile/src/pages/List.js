2d22a6fff90aebd455ab53c6b188e7e57fd714ba
Gabriel Gigante <gabriel_gigante@outlook.com>
2019-10-04 13:07:02 -0300

    Adding the real time feature

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, Image, AsyncStorage, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { Alert, SafeAreaView, Text, Image, AsyncStorage, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+
+import socketio from 'socket.io-client';
 
 import SpotList from '../components/SpotList';
 
@@ -8,6 +10,18 @@ import logo from '../assets/logo.png';
 export default function List({ navigation }) {
 
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.0.100:3333', {
+        query: { user_id }
+      })
+
+      socket.on('booking_response', booking => {        
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+      })
+    })
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem('techs').then(storagedTechs => {
